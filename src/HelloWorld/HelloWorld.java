92f0ae1e4929e17926d6c557d25a0bb3fc50dd25
snake <admin@viasnake.com>
2021-11-18 11:11:29 +0900

    HelloJava

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -1,6 +1,6 @@
-package Hello;
+package HelloWorld;
 
-public class main {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
